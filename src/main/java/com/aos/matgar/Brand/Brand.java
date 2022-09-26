af0a380141d1175a8875f617be7ac1aa7a2d7afa
Abubakr_Hamdy <abubakr.mohamed@datagearbi.com>
2022-09-26 21:49:26 +0200

    add brand class

---
+++ src/main/java/com/aos/matgar/Brand/Brand.java
@@ -0,0 +1,12 @@
+package main.java.com.aos.matgar.Brand.Entity;
+
+import main.java.com.aos.matgar.Product.Entity.Product;
+
+import java.util.Set;
+
+public class Brand {
+    String ID;
+    String name;
+    String shortDescription;
+    Set<Product> products;
+}
