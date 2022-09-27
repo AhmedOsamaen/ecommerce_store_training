bce19341ed70bd10ea6acc934009615a16360774
Abubakr_Hamdy <abubakr.mohamed@datagearbi.com>
2022-09-27 19:59:59 +0200

    Add (products,brand,categories)

--- src/main/java/com/aos/matgar/Brand/Brand.java
+++ src/main/java/com/aos/matgar/Brand/Brand.java
@@ -1,12 +1,25 @@
-package main.java.com.aos.matgar.Brand.Entity;
+package com.aos.matgar.Brand;
 
-import main.java.com.aos.matgar.Product.Entity.Product;
+import com.aos.matgar.Product.Product;
+import lombok.Data;
 
+import javax.persistence.*;
 import java.util.Set;
-
+@Entity
+@Data
+@Table(name = "BRAND")
 public class Brand {
-    String ID;
+    @Id
+    @GeneratedValue
+    @Column(name = "brand_id")
+    String id;
     String name;
     String shortDescription;
+    @ManyToMany
+    @JoinTable(
+            name = "brands_products",
+            joinColumns = @JoinColumn(name = "brand_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
     Set<Product> products;
+
 }
--- src/main/java/com/aos/matgar/Product/Product.java
+++ src/main/java/com/aos/matgar/Product/Product.java
@@ -1,45 +1,56 @@
 package com.aos.matgar.Product;
 
+import com.aos.matgar.Brand.Brand;
+import com.aos.matgar.Category.Category;
 import com.aos.matgar.Order.Order;
+
 import com.aos.matgar.Store.Store;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 
 import javax.persistence.*;
+import java.sql.Date;
+import java.util.HashSet;
 import java.util.Set;
-
+@Data
 @Entity
-@Table(name = "PRODUCT")
 public class Product {
     @Id
     @GeneratedValue
     @Column(name = "product_id")
     private long id;
+    String name;
+    @Column(name = "short_description")
+    String shortDescription;//it will have more info for the product
+    @Column(name = "long_description")
+    String longDescription;//width,height,warranty,
+    int price;
+    int quantity;
+    //will make discount a class but not a table
+    int discount;// 20=20%
+    int maximumDiscountValue;
+    Date discountExpirationDate;
+    /////////
+    String images;//delimited by ;
+    String model;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "products")
+    Set<Order> orders;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "products")
+    Set<Brand> brands= new HashSet<>();
 
 
     @JsonIgnore
     @ManyToMany(mappedBy = "products")
-    private Set<Store> stores;
+    Set<Category> categories;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @JsonIgnore
-    @ManyToMany(mappedBy = "products")
-    private Set<Order> orders;
-
-    public Product() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Set<Store> getStores() {
-        return stores;
-    }
-
-    public void setStores(Set<Store> stores) {
-        this.stores = stores;
-    }
+    @JoinColumn(name = "store_id", referencedColumnName = "store_id")
+    Store store;
+    //    Set<Review> reviews;
+    Product(){};
 }
